4cda07ef5fdc9bd81d23dd6a055005677b987303
Duy Nguyen <dnguyen0304@gmail.com>
2022-07-30 17:50:52 -0600

    Document TODO for Transition theme component.

--- src/theme/components/Transition/index.tsx
+++ src/theme/components/Transition/index.tsx
@@ -1,6 +1,8 @@
+// TODO(dnguyen0304): Investigate why moving this module to src/components
+// throws "ReferenceError: exports is not defined".
+
 // Copied from:
 // https://mui.com/material-ui/react-dialog/#transitions
-
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 import * as React from 'react';
