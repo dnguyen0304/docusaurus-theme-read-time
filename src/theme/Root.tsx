32d9ff11b11db40e5e1c3c9194317146a64ec584
Duy Nguyen <dnguyen0304@gmail.com>
2022-07-30 15:33:34 -0600

    Add COLOR_GREY_700 for theme.

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -8,13 +8,26 @@ interface Props {
 }
 
 const COLOR_ACCENT_GREEN: string = '#64ffda';
+// TODO(dnguyen0304): Rename to COLOR_GREY_600.
 const COLOR_BLUEISH_BLACK: string = '#2e4561';
+const COLOR_GREY_700: string = '#363732';
 
 const theme = createTheme({
     palette: {
         primary: {
             main: COLOR_ACCENT_GREEN,
             contrastText: COLOR_BLUEISH_BLACK,
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                // See Button CSS class name documentation:
+                // https://mui.com/material-ui/api/button/#css
+                textPrimary: {
+                    color: COLOR_GREY_700,
+                },
+            },
         },
     },
 });
