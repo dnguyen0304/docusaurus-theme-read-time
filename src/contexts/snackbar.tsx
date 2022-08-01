8853fd024286a9480a2ba5959505a02eddacdd86
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-01 12:23:23 -0600

    Add Snackbar context.

---
+++ src/contexts/snackbar.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { SnackbarType } from '../theme/services/Snackbar';
+import { ReactContextError } from './errors';
+
+interface ContextValue {
+    readonly snackbar: SnackbarType,
+};
+
+const Context = React.createContext<ContextValue | undefined>(undefined);
+
+function useContextValue(snackbar: SnackbarType): ContextValue {
+    return React.useMemo(
+        () => ({ snackbar }),
+        [snackbar],
+    );
+}
+
+interface Props {
+    readonly snackbar: SnackbarType,
+    readonly children: React.ReactNode,
+};
+
+function SnackbarProvider({ snackbar, children }: Props): JSX.Element {
+    const value = useContextValue(snackbar);
+
+    return (
+        <Context.Provider value={value}>
+            {children}
+        </Context.Provider>
+    );
+};
+
+function useSnackbar(): ContextValue {
+    const context = React.useContext(Context);
+    if (context === undefined) {
+        throw new ReactContextError('SnackbarProvider');
+    }
+    return context;
+}
+
+export {
+    SnackbarProvider,
+    useSnackbar,
+};
