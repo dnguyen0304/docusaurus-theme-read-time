b596403a14a1f473b07abbc38e305e601db17327
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-01 12:50:51 -0600

    Add done icon transition for EditModeSaveButton theme component.

--- src/theme/Editor/EditMode/ButtonGroup/SaveButton/index.tsx
+++ src/theme/Editor/EditMode/ButtonGroup/SaveButton/index.tsx
@@ -1,3 +1,4 @@
+import DoneIcon from '@mui/icons-material/Done';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
@@ -17,6 +18,8 @@ export default function SaveButton(
     }: Props
 ): JSX.Element {
     const snackbar = useSnackbar().snackbar;
+    const [isConfirmed, setIsConfirmed] = React.useState<boolean>(false);
+    const timerId = React.useRef<number>();
 
     // TODO(dnguyen0304): Investigate if a real implementation is necessary in
     // addition to background saving.
@@ -25,9 +28,33 @@ export default function SaveButton(
         new Promise(resolve => setTimeout(resolve, 750))
             .then(() => {
                 setIsSaving(false);
+                setIsConfirmed(true);
                 snackbar.sendSuccessAlert('Successfully saved changes.');
+            })
+            .then(() => {
+                timerId.current = window.setTimeout(() => {
+                    setIsConfirmed(false);
+                }, 1000);
             });
     };
+
+    const getIcon = (): JSX.Element => {
+        if (isConfirmed) {
+            return <DoneIcon color='primary' />;
+        } else if (isSaving) {
+            // All components must have the same dimensions (size) to avoid
+            // visual studdering.
+            return <CircularProgress size={24} />;
+        } else {
+            return <SaveOutlinedIcon />;
+        };
+    };
+
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(timerId.current);
+        };
+    }, []);
 
     return (
         <Tooltip
@@ -38,11 +65,7 @@ export default function SaveButton(
                 aria-label='save'
                 onClick={handleClick}
             >
-                {isSaving
-                    // All components must have the same dimensions (size) to
-                    // avoid visual studdering.
-                    ? <CircularProgress size={24} />
-                    : <SaveOutlinedIcon />}
+                {getIcon()}
             </IconButton>
         </Tooltip>
     );
