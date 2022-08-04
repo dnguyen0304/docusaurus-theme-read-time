0fafdd32f969d7ed2111623f8822abe3fdd6a8c0
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-04 01:13:04 -0600

    Change position style to sticky for EditorContainer theme component.

--- src/theme/Editor/Container/index.tsx
+++ src/theme/Editor/Container/index.tsx
@@ -14,6 +14,16 @@ export default function Container({ children }: Props): JSX.Element {
         // boundByDirection props.
         <Resizable
             className={styles.editorResizableContainer}
+            style={{
+                // Set the position as an inline style to override the
+                // specificity of a workaround implemented by the underlying
+                // code.
+                // See https://github.com/bokuweb/re-resizable/blob/0e9055cb1fe80ea824d78061961d76b0a4fadb8d/src/index.tsx#L931
+                //
+                // Warning: width and height styles are always overridden.
+                // See https://github.com/bokuweb/re-resizable/blob/0e9055cb1fe80ea824d78061961d76b0a4fadb8d/src/index.tsx#L934
+                position: 'sticky',
+            }}
             enable={{
                 left: true,
             }}
