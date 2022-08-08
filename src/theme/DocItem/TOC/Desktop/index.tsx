3c1d8eb7001c6698f942ab334e60998c06a97705
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-07 21:54:35 -0600

    Add GithubProvider for DesktopWrapper theme component.

--- src/theme/DocItem/TOC/Desktop/index.tsx
+++ src/theme/DocItem/TOC/Desktop/index.tsx
@@ -3,6 +3,7 @@ import Desktop from '@theme-init/DocItem/TOC/Desktop';
 import type DesktopType from '@theme/DocItem/TOC/Desktop';
 import React from 'react';
 import { useEditor } from '../../../../contexts/editor';
+import { GithubProvider } from '../../../../contexts/github';
 import Editor from '../../../Editor';
 
 type Props = WrapperProps<typeof DesktopType>;
@@ -10,10 +11,16 @@ type Props = WrapperProps<typeof DesktopType>;
 export default function DesktopWrapper(props: Props): JSX.Element {
     const context = useEditor();
 
+    const editor = (
+        <GithubProvider>
+            <Editor />
+        </GithubProvider>
+    );
+
     return (
         <>
             {context.editorIsOpen
-                ? <Editor />
+                ? editor
                 : <Desktop {...props} />}
         </>
     );
