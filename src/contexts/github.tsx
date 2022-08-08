b154407be0d343396402447838f0c97833049367
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-07 23:56:36 -0600

    Add api and setApi for github context.

--- src/contexts/github.tsx
+++ src/contexts/github.tsx
@@ -1,19 +1,23 @@
+import type { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
 import * as React from 'react';
 import { ReactContextError } from './errors';
 
 interface ContextValue {
+    readonly api: RestEndpointMethodTypes | undefined;
     readonly username: string;
+    readonly setApi: React.Dispatch<React.SetStateAction<RestEndpointMethodTypes | undefined>>;
     readonly setUsername: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const Context = React.createContext<ContextValue | undefined>(undefined);
 
 function useContextValue(): ContextValue {
+    const [api, setApi] = React.useState<RestEndpointMethodTypes>();
     const [username, setUsername] = React.useState<string>('');
 
     return React.useMemo(
-        () => ({ username, setUsername }),
-        [username, setUsername],
+        () => ({ api, username, setApi, setUsername }),
+        [api, username, setApi, setUsername],
     );
 }
 
