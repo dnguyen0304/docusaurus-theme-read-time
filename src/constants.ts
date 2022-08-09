1720860f81502d625679dd3f36e22b72a641c1c7
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-08 21:59:19 -0600

    Add site context.

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,13 @@
+// See https://github.com/facebook/docusaurus/blob/01ac2e0fcaccaf469992f93a0e8bf04e61cf850e/packages/docusaurus-utils/src/pathUtils.ts#L93
+const DOCUSAURUS_ALIASED_SITE_PATH_PREFIX: string = '@site';
+
 // TODO(dnguyen0304): Extract as a configuration option.
 const ENDPOINT_EXCHANGE_CODE_TO_TOKEN: string = 'https://z39iitznuj.execute-api.us-west-1.amazonaws.com/production/DocusaurusThemeEditor_getAccessToken';
 
 const GITHUB_AUTHORIZATION_CALLBACK_PATH: string = '/editor/callback';
 
 export {
+    DOCUSAURUS_ALIASED_SITE_PATH_PREFIX,
     ENDPOINT_EXCHANGE_CODE_TO_TOKEN,
     GITHUB_AUTHORIZATION_CALLBACK_PATH,
 };
---
+++ src/contexts/site.tsx
@@ -0,0 +1,31 @@
+// TODO(dnguyen0304): Fix missing type declarations.
+import { useDoc } from '@docusaurus/theme-common/internal';
+import URI from 'urijs';
+import { DOCUSAURUS_ALIASED_SITE_PATH_PREFIX } from '../constants';
+
+interface ContextValue {
+    readonly owner: string;
+    readonly repository: string;
+    readonly path: string;
+};
+
+function useSite(): ContextValue {
+    const {
+        metadata: {
+            editUrl,
+            source,
+        },
+    } = useDoc();
+
+    const [siteOwner, siteRepository] = new URI(editUrl).segment();
+
+    return {
+        owner: siteOwner,
+        repository: siteRepository,
+        path: source.replace(`${DOCUSAURUS_ALIASED_SITE_PATH_PREFIX}/`, ''),
+    }
+}
+
+export {
+    useSite,
+};
