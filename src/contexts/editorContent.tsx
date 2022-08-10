eb4f089073d516d1161bd99f18bc35fb340c886d
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-10 12:58:41 -0600

    Add editorTabs for editorContent context.

--- src/contexts/editorContent.tsx
+++ src/contexts/editorContent.tsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
 import { ReactContextError } from './errors';
 
+// TODO(dnguyen0304): Add lastUpdatedAt.
+interface EditorTab {
+    pullRequestUrl: string;
+}
+
 // TODO(dnguyen0304): Import from docusaurus-plugin-editor.
 interface EditorContent {
     [key: string]: string;
 }
 
 interface ContextValue {
+    readonly editorTabs: EditorTab[];
     readonly editorContent: EditorContent;
+    readonly setEditorTabs: React.Dispatch<React.SetStateAction<EditorTab[]>>
     readonly setEditorContent: React.Dispatch<React.SetStateAction<EditorContent>>;
 };
 
 const Context = React.createContext<ContextValue | undefined>(undefined);
 
 function useContextValue(content: EditorContent): ContextValue {
+    const [editorTabs, setEditorTabs] = React.useState<EditorTab[]>([]);
     const [editorContent, setEditorContent] =
         React.useState<EditorContent>(content);
 
     return React.useMemo(
-        () => ({ editorContent, setEditorContent }),
-        [editorContent, setEditorContent],
+        () => ({ editorTabs, editorContent, setEditorTabs, setEditorContent }),
+        [editorTabs, editorContent, setEditorTabs, setEditorContent],
     );
 }
 
