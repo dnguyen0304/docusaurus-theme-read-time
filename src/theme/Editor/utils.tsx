b8c5a71defacd14d101ec71faf9af2e88fa88f33
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-10 02:26:38 -0600

    Add useRefMeasure util.

--- src/theme/Editor/utils.tsx
+++ src/theme/Editor/utils.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { ContextValue } from '../../contexts/site';
 
 export const getLocalStorageKey = (
@@ -8,4 +9,18 @@ export const getLocalStorageKey = (
     }: ContextValue
 ): string => {
     return `${owner}/${repository}/${path}`;
+}
+
+export const useRefMeasure = <T extends HTMLElement>(
+    callback: (clientRect: DOMRect) => number
+) => {
+    const [measure, setMeasure] = React.useState<number | undefined>();
+
+    const measureRef = React.useCallback((node: T) => {
+        if (node !== null) {
+            setMeasure(callback(node.getBoundingClientRect()));
+        }
+    }, []);
+
+    return [measure, measureRef];
 }
