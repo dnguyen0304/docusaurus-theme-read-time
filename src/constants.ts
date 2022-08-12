64da0f7603e4b8b2d97f7c2baa0915b6697ada15
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-12 15:42:40 -0600

    Revert add workaround for callback losing search params for Editor/Callback theme component.

--- src/constants.ts
+++ src/constants.ts
@@ -4,14 +4,8 @@ const DOCUSAURUS_ALIASED_SITE_PATH_PREFIX: string = '@site';
 // TODO(dnguyen0304): Extract as a configuration option.
 const ENDPOINT_EXCHANGE_CODE_TO_TOKEN: string = 'https://z39iitznuj.execute-api.us-west-1.amazonaws.com/production/DocusaurusThemeEditor_getAccessToken';
 
-// TODO(dnguyen0304): Investigate S3 static website redirect workaround.
-//
-// If the S3 object key "foo" is not found, the user is redirected to "foo/",
-// "foo/index.html" is served, but search parameters are removed. In
-// Chrome DevTools | Network, this is observed as a 302 Found redirect.
-//
-// See: https://docs.aws.amazon.com/AmazonS3/latest/userguide/IndexDocumentSupport.html
-// See: https://stackoverflow.com/questions/21468600/amazon-s3-static-website-hosting-support-both-trailing-slash-and-not
+// Keep in sync:
+// GitHub > Developer Settings > OAuth Apps > "Authorization callback URL"
 const GITHUB_AUTHORIZATION_CALLBACK_PATH: string = '/editor/callback/';
 
 export {
