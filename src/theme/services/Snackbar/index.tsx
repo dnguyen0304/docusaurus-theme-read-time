142a8ad03b9416809b03b6a3d3127a4cb45ea61b
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-12 23:25:17 -0600

    Add durationUpdater for Snackbar service.

--- src/theme/services/Snackbar/index.tsx
+++ src/theme/services/Snackbar/index.tsx
@@ -10,9 +10,19 @@ import * as React from 'react';
 
 export interface SnackbarType {
     create: () => JSX.Element;
-    sendAlert: (severity: AlertColor, content: React.ReactNode) => void;
-    sendSuccessAlert: (content: React.ReactNode) => void;
-    sendWarningAlert: (content: React.ReactNode) => void;
+    sendAlert: (
+        severity: AlertColor,
+        content: React.ReactNode,
+        durationUpdater: (prev: number) => number,
+    ) => void;
+    sendSuccessAlert: (
+        content: React.ReactNode,
+        durationUpdater?: (prev: number) => number,
+    ) => void;
+    sendWarningAlert: (
+        content: React.ReactNode,
+        durationUpdater?: (prev: number) => number,
+    ) => void;
 }
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
@@ -32,6 +42,8 @@ export default function Snackbar(): SnackbarType {
     const [isOpen, setIsOpen] = React.useState<boolean>(false);
     const [content, setContent] =
         React.useState<JSX.Element | undefined>(undefined);
+    const [duration, setDuration] =
+        React.useState<number>(10 * 1000);  // 10 seconds in milliseconds
 
     const create = (): JSX.Element => {
         return (
@@ -41,7 +53,7 @@ export default function Snackbar(): SnackbarType {
                     horizontal: 'left',
                 }}
                 // TODO(dnguyen0304): Extract as a configuration option.
-                autoHideDuration={10 * 1000}  // 10 seconds in milliseconds
+                autoHideDuration={duration}
                 onClose={close}
                 open={isOpen}
             >
@@ -63,18 +75,35 @@ export default function Snackbar(): SnackbarType {
         setIsOpen(false);
     };
 
-    const sendAlert = (severity: AlertColor, content: React.ReactNode) => {
+    const sendAlert = (
+        severity: AlertColor,
+        content: React.ReactNode,
+        durationUpdater: (prev: number) => number,
+    ) => {
+        const previousDuration = duration;
+        setDuration(durationUpdater);
         open(
             <Alert severity={severity} onClose={close}>
                 {content}
             </Alert>
         );
+        setDuration(previousDuration);
     };
-    const sendSuccessAlert = (content: React.ReactNode) => {
-        sendAlert('success', content);
+    const sendSuccessAlert = (
+        content: React.ReactNode,
+        durationUpdater: (prev: number) => number = noOpUpdater,
+    ) => {
+        sendAlert('success', content, durationUpdater);
     };
-    const sendWarningAlert = (content: React.ReactNode) => {
-        sendAlert('warning', content);
+    const sendWarningAlert = (
+        content: React.ReactNode,
+        durationUpdater: (prev: number) => number = noOpUpdater,
+    ) => {
+        sendAlert('warning', content, durationUpdater);
+    };
+
+    const noOpUpdater = (prev: number): number => {
+        return prev;
     };
 
     return {
