69aa480717d0d7b33ef9dc1dead6a2bb37ab61ed
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-17 00:52:08 -0600

    Remove setLocalStorageObject and clearLocalStorageObject for Editor util.

--- src/theme/Editor/utils.tsx
+++ src/theme/Editor/utils.tsx
@@ -11,34 +11,6 @@ const getLocalStorageKey = (
     return `${owner}/${repository}/${path}`;
 };
 
-const setLocalStorageObject = <T,>(
-    localStorageKey: string,
-    objectKey: keyof T,
-    objectValue: string,
-) => {
-    let parsed: T = {} as T;
-
-    const item = localStorage.getItem(localStorageKey);
-    if (item !== null && item === '') {
-        parsed = JSON.parse(item);
-    }
-
-    // TODO(dnguyen00304): Investigate type error.
-    parsed[objectKey] = objectValue;
-    localStorage.setItem(localStorageKey, JSON.stringify(parsed));
-};
-
-const clearLocalStorageObject = <T,>(
-    localStorageKey: string,
-    objectKey: keyof T,
-) => {
-    const item = localStorage.getItem(localStorageKey);
-    if (item === null || item === '') {
-        return;
-    }
-    setLocalStorageObject(localStorageKey, objectKey, '');
-};
-
 const useRefMeasure = <T extends HTMLElement>(
     callback: (clientRect: DOMRect) => number,
     deps: ReadonlyArray<unknown> = [],
@@ -55,8 +27,6 @@ const useRefMeasure = <T extends HTMLElement>(
 };
 
 export {
-    clearLocalStorageObject,
     getLocalStorageKey,
-    setLocalStorageObject,
     useRefMeasure,
 };
