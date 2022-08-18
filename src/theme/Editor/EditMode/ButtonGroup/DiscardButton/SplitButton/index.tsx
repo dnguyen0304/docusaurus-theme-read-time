71aa5ae5cb9727c46126a7f15caac08f58419f66
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-18 09:49:18 -0600

    Fix missing import for EditMode/SplitButton theme component.

--- src/theme/Editor/EditMode/ButtonGroup/DiscardButton/SplitButton/index.tsx
+++ src/theme/Editor/EditMode/ButtonGroup/DiscardButton/SplitButton/index.tsx
@@ -10,6 +10,7 @@ import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
+import { LOCAL_STORAGE_KEY_PULL_TITLE } from '../../../../../../constants';
 import { useEditor } from '../../../../../../contexts/editor';
 import { useGithub } from '../../../../../../contexts/github';
 import { useSite } from '../../../../../../contexts/site';
