948dcce45b3f05697d8c90c6f564ce49f93bb982
Duy Nguyen <dnguyen0304@gmail.com>
2022-08-19 09:51:37 -0600

    Refactor for EditMode/ProposeButton theme component.

--- src/theme/Editor/EditMode/ButtonGroup/ProposeButton/index.tsx
+++ src/theme/Editor/EditMode/ButtonGroup/ProposeButton/index.tsx
@@ -240,9 +240,9 @@ export default function ProposeButton(
         const item = localStorage.getItem(LOCAL_STORAGE_KEY_PULL_TITLE);
         if (item === null) {
             setPullTitle('');
-            return;
+        } else {
+            setPullTitle(item);
         }
-        setPullTitle(item);
     }, []);
 
     return (
