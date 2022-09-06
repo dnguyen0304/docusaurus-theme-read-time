3f3ee533e3a9376908470cdf45fe204c4ae17aa8
Duy Nguyen <dnguyen0304@gmail.com>
2022-09-06 14:46:51 -0500

    Fix missing maxWidth for Editor/Container theme component.

--- src/theme/Editor/Container/index.tsx
+++ src/theme/Editor/Container/index.tsx
@@ -1,13 +1,42 @@
 import { Resizable } from 're-resizable';
 import * as React from 'react';
-import EditorHandle, { WIDTH_PX } from '../Handle';
+import EditorHandle, { WIDTH_PX as HANDLE_WIDTH_PX } from '../Handle';
 import styles from './styles.module.css';
+
+const WIDTH_IDEAL_PX: number = 400;
+const WIDTH_MAX_PERCENT: number = .40;
+const PADDING_PX: number = 16;
 
 type Props = {
     readonly children: React.ReactNode;
 }
 
 export default function Container({ children }: Props): JSX.Element {
+    const [maxWidth, setMaxWidth] = React.useState<number>(WIDTH_IDEAL_PX);
+
+    // TODO(dnguyen0304): Trigger on resize.
+    React.useEffect(() => {
+        const containerWidth =
+            document
+                .querySelector(`main[class*='docMainContainer']`)
+                ?.getBoundingClientRect()
+                .width;
+        if (!containerWidth || containerWidth < WIDTH_IDEAL_PX) {
+            setMaxWidth(WIDTH_IDEAL_PX);
+        } else {
+            setMaxWidth(
+                containerWidth * WIDTH_MAX_PERCENT
+                // The resizable container contains a left handle, center
+                // content, and right padding. maxWidth refers to the width of
+                // the center content. Subtract the width of the handle and
+                // padding so the content does not overflow.
+                - HANDLE_WIDTH_PX
+                - PADDING_PX
+                // Subtract an additional buffer to fix the visual stutter.
+                - 1);
+        }
+    }, []);
+
     return (
         // TODO(dnguyen0304): Investigate how to set minWidth and maxWidth.
         // Unconfirmed: It might be related to the undocumented bounds and
@@ -35,10 +64,11 @@ export default function Container({ children }: Props): JSX.Element {
             // handleWrapper and handleComponent.
             handleStyles={{
                 left: {
-                    width: `${WIDTH_PX}px`,
+                    width: `${HANDLE_WIDTH_PX}px`,
                     left: 0,
                 },
             }}
+            maxWidth={`${maxWidth}px`}
             // TODO(dnguyen0304): Replace placeholder default value.
             minWidth='275px'
         >
