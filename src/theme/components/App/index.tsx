16edfdca33ac8f6a5dce51177b9c13b5db2845fb
Duy Nguyen <dnguyen0304@gmail.com>
2022-09-25 02:03:13 -0500

    Extract to setCookieWithFallback for App theme component.

--- src/theme/components/App/index.tsx
+++ src/theme/components/App/index.tsx
@@ -2,12 +2,29 @@
 // theme/DocItem/Layout.
 
 import * as React from 'react';
-import Cookies from 'universal-cookie';
+import Cookies, { CookieSetOptions } from 'universal-cookie';
 import URI from 'urijs';
 import {
     COOKIE_KEY_SESSION_ID,
     SEARCH_PARAM_KEY_AUTH
 } from '../../../constants';
+
+function setCookieWithFallback(
+    name: string,
+    value: any,
+    options?: CookieSetOptions | undefined,
+): void {
+    // Set the cookie twice. First without options and second with them.
+    // This fallback is necessary for hosting providers such as AWS S3 that
+    // do not support cookie attributes (reproduced but no source
+    // documentation). For example:
+    //
+    //   document.cookie = 'without_options=works'
+    //   document.cookie = 'with_options=does_not_work;Secure'
+    const cookies = new Cookies();
+    cookies.set(name, value);
+    cookies.set(name, value, options);
+};
 
 export default function App(): null {
     // TODO(dnguyen0304): Move to Editor/Callback to hide the refresh.
@@ -18,17 +35,8 @@ export default function App(): null {
         if (!encodedAuth) {
             return;
         }
-        const cookies = new Cookies();
         const auth = JSON.parse(atob(encodedAuth));
-        // Set the cookie twice. First without options and second with them.
-        // This fallback is necessary for hosting providers such as AWS S3 that
-        // do not support cookie attributes (reproduced but no source
-        // documentation). For example:
-        //
-        //   document.cookie = 'without_options=works'
-        //   document.cookie = 'with_options=does_not_work;Secure'
-        cookies.set(COOKIE_KEY_SESSION_ID, auth.accessToken);
-        cookies.set(
+        setCookieWithFallback(
             COOKIE_KEY_SESSION_ID,
             auth.accessToken,
             // TODO(dnguyen0304): Investigate if path is necessary.
