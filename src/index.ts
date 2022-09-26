9644d3f0cd3b5b334de5a0e5f4f4e5e46d45a3de
Duy Nguyen <dnguyen0304@gmail.com>
2022-09-26 10:23:08 -0500

    Fix type for themeEditor index.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from '@docusaurus/types';
 
-export default function themeEditor(): Plugin<void> {
+export default function themeEditor(): Plugin<undefined> {
     return {
         name: 'docusaurus-theme-editor',
 
