bf23f956d999348e11b9d79b9a182906bea4ffd5
Duy Nguyen <dnguyen0304@gmail.com>
2022-09-30 11:40:19 -0500

    Update default band.colors for theme config.

--- src/validateThemeConfig.ts
+++ src/validateThemeConfig.ts
@@ -17,11 +17,11 @@ export const DOCUPOTAMUS_DEFAULT_CONFIG = {
                 // implemented as box shadows and therefore stack.
                 // See: https://colorbox.io/?c0=%26p%24s%24%3D11%26p%24h%24st%24%3D351%26p%24h%24e%24%3D353%26p%24h%24c%24%3Deqo%26p%24sa%24st%24%3D0.08%26p%24sa%24e%24%3D0.94%26p%24sa%24r%24%3D1%26p%24sa%24c%24%3Deqo%26p%24b%24st%24%3D1%26p%24b%24e%24%3D0.04%26p%24b%24c%24%3Dl%26o%24n%24%3DNew+Color%26o%24ro%24%3Dcw%26o%24ms%24%3D0%2C1
                 colors: [
-                    'hsla(352, 40%, 52%, 0.5)',  // B2
+                    'hsla(352, 46%, 65%, 0.6)',  // B2
                     'hsla(352, 48%, 42%, 0.6)',  // B1
                     'hsla(353, 59%, 33%, 0.7)',  // B0
                     'hsla(352, 48%, 42%, 0.6)',  // B1
-                    'hsla(352, 40%, 52%, 0.5)',  // B2
+                    'hsla(352, 46%, 65%, 0.6)',  // B2
                 ],
             },
             border: {
