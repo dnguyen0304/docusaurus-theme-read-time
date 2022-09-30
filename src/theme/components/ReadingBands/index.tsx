e9bd3d93d3318f49c3f695de055f376328c1e470
Duy Nguyen <dnguyen0304@gmail.com>
2022-09-29 20:57:17 -0500

    Remove unused getViewportWidth for ReadingBands theme component.

--- src/theme/components/ReadingBands/index.tsx
+++ src/theme/components/ReadingBands/index.tsx
@@ -33,13 +33,6 @@ const bands: Band[] = [
     },
 ];
 
-function getViewportWidth(): number {
-    return Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0,
-    );
-}
-
 export default function ReadingBands(
     {
     }: Props
