74a84ef1447b7fb6af51b2c50a691d64e2268f65
Duy Nguyen <dnguyen0304@gmail.com>
2022-11-25 07:29:46 -0700

    Migrate to inline export for constants.

--- src/constants.ts
+++ src/constants.ts
@@ -1,26 +1,18 @@
-type LOCAL_STORAGE_KEYS = 'markdown' | 'pull_branch_name' | 'pull_title' | 'pull_url';
+export type LOCAL_STORAGE_KEYS =
+    | 'markdown'
+    | 'pull_branch_name'
+    | 'pull_title'
+    | 'pull_url';
 
 // See https://github.com/facebook/docusaurus/blob/01ac2e0fcaccaf469992f93a0e8bf04e61cf850e/packages/docusaurus-utils/src/pathUtils.ts#L93
-const DOCUSAURUS_ALIASED_SITE_PATH_PREFIX: string = '@site';
+export const DOCUSAURUS_ALIASED_SITE_PATH_PREFIX: string = '@site';
 
 // Keep in sync:
 // GitHub > Developer Settings > OAuth Apps > "Authorization callback URL"
-const SEARCH_PARAM_KEY_AUTH: string = 'auth';
-const SEARCH_PARAM_KEY_LOGGED_IN_AT: string = 'logged_in_at';
-const COOKIE_KEY_SESSION_ID: string = 'session_id';
-const LOCAL_STORAGE_KEY_MARKDOWN: LOCAL_STORAGE_KEYS = 'markdown';
-const LOCAL_STORAGE_KEY_PULL_BRANCH_NAME: LOCAL_STORAGE_KEYS = 'pull_branch_name';
-const LOCAL_STORAGE_KEY_PULL_TITLE: LOCAL_STORAGE_KEYS = 'pull_title';
-const LOCAL_STORAGE_KEY_PULL_URL: LOCAL_STORAGE_KEYS = 'pull_url';
-
-export {
-    COOKIE_KEY_SESSION_ID,
-    DOCUSAURUS_ALIASED_SITE_PATH_PREFIX,
-    LOCAL_STORAGE_KEY_MARKDOWN,
-    LOCAL_STORAGE_KEY_PULL_BRANCH_NAME,
-    LOCAL_STORAGE_KEY_PULL_TITLE,
-    LOCAL_STORAGE_KEY_PULL_URL,
-    LOCAL_STORAGE_KEYS,
-    SEARCH_PARAM_KEY_AUTH,
-    SEARCH_PARAM_KEY_LOGGED_IN_AT,
-};
+export const SEARCH_PARAM_KEY_AUTH: string = 'auth';
+export const SEARCH_PARAM_KEY_LOGGED_IN_AT: string = 'logged_in_at';
+export const COOKIE_KEY_SESSION_ID: string = 'session_id';
+export const LOCAL_STORAGE_KEY_MARKDOWN: LOCAL_STORAGE_KEYS = 'markdown';
+export const LOCAL_STORAGE_KEY_PULL_BRANCH_NAME: LOCAL_STORAGE_KEYS = 'pull_branch_name';
+export const LOCAL_STORAGE_KEY_PULL_TITLE: LOCAL_STORAGE_KEYS = 'pull_title';
+export const LOCAL_STORAGE_KEY_PULL_URL: LOCAL_STORAGE_KEYS = 'pull_url';
