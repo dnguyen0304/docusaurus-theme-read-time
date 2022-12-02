80523fcba14ef88b78244d4912e9d5954b940199
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-02 13:22:18 +0100

    Fix missing type declaration for DocPage/Layout/Main theme component.

--- src/docusaurus-theme-read-time.d.ts
+++ src/docusaurus-theme-read-time.d.ts
@@ -1,6 +1,8 @@
-declare module '*.module.css' {
-    const classes: {
-        [key: string]: string,
-    };
-    export default classes;
+// TODO(dnguyen0304): Investigate referencing @docusaurus/module-type-aliases.
+/// <reference types="@docusaurus/theme-classic" />
+
+declare module '@theme-init/DocPage/Layout/Main' {
+    import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
+
+    export default DocPageLayoutMain;
 }
--- src/theme/DocPage/Layout/Main/index.tsx
+++ src/theme/DocPage/Layout/Main/index.tsx
@@ -1,5 +1,4 @@
 import type { WrapperProps } from '@docusaurus/types';
-// TODO(dnguyen0304): Fix missing type declaration.
 import DocPageLayoutMain from '@theme-init/DocPage/Layout/Main';
 import type DocPageLayoutMainType from '@theme/DocPage/Layout/Main';
 import * as React from 'react';
