ab0fc58abade9b803e04d0e3341cc5712130690a
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-04 08:48:59 +0100

    Move to components for Toolbar theme component.

---
+++ src/components/Toolbar/index.tsx
@@ -0,0 +1,33 @@
+import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import * as React from 'react';
+import { useToolbar } from '../../contexts/toolbar';
+
+type Props = Readonly<{}>;
+
+export default function Toolbar(
+    {
+    }: Props
+): JSX.Element {
+    const { setWorkbenchIsOpen } = useToolbar();
+
+    return (
+        <Box
+            // TODO(dnguyen0304): Add paddingRight for the scrollbar.
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '110px',
+                borderLeft: '1px solid var(--ifm-toc-border-color)',
+                paddingTop: '1rem',
+            }}
+        >
+            {/* TODO(dnguyen0304): Add real onClick implementation. */}
+            <IconButton onClick={() => { setWorkbenchIsOpen(prev => !prev) }}>
+                <InsightsOutlinedIcon />
+            </IconButton>
+        </Box>
+    );
+}
--- src/theme/DocPage/Layout/Main/index.tsx
+++ src/theme/DocPage/Layout/Main/index.tsx
@@ -2,7 +2,7 @@ import type { WrapperProps } from '@docusaurus/types';
 import DocPageLayoutMain from '@theme-init/DocPage/Layout/Main';
 import type DocPageLayoutMainType from '@theme/DocPage/Layout/Main';
 import * as React from 'react';
-import Toolbar from '../../../components/Toolbar';
+import Toolbar from '../../../../components/Toolbar';
 
 type Props = Readonly<WrapperProps<typeof DocPageLayoutMainType>>;
 
