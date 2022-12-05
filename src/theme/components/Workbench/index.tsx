0665ae5908f7453e489c7b4c57476e1419e438a6
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-05 14:45:01 +0100

    Add border-radius style for Workbench theme component.

--- src/theme/components/Workbench/index.tsx
+++ src/theme/components/Workbench/index.tsx
@@ -48,7 +48,8 @@ export default function Workbench(
                     0px 4px 5px 0px rgb(0 0 0 / 14%),
                     0px 1px 10px 0px rgb(0 0 0 / 12%),
                     0px 2px 4px -1px rgb(0 0 0 / 20%)`,
-                padding: '1rem .7rem',
+                padding: '1rem 0.7rem',
+                borderRadius: '0.6rem 0 0 0.6rem',
                 '& > *': {
                     marginBottom: '1rem',
                 },
