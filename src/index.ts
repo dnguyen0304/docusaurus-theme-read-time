519e7a99ec21d3b732d857f6a416daf8fcd74412
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-05 08:12:48 +0100

    Document TODO for themeReadTime.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { Plugin } from '@docusaurus/types';
 
+// TODO(dnguyen0304): Fix missing LoadedContent type declaration.
 export default function themeReadTime(): Plugin<undefined> {
     return {
         name: 'docusaurus-theme-read-time',
