248d1c98e80e9666e83c91651be59f1db11df9c3
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-05 14:27:32 +0100

    Add padding style for Workbench theme component.

--- src/theme/components/Workbench/index.tsx
+++ src/theme/components/Workbench/index.tsx
@@ -48,6 +48,7 @@ export default function Workbench(
                     0px 4px 5px 0px rgb(0 0 0 / 14%),
                     0px 1px 10px 0px rgb(0 0 0 / 12%),
                     0px 2px 4px -1px rgb(0 0 0 / 20%)`,
+                padding: '1rem .7rem',
                 '& > *': {
                     marginBottom: '1rem',
                 },
