0ab87db7272020f9225b11210592ea516210aca9
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-08 17:06:26 +0100

    Add StyledBox for Workbench/Card theme component.

--- src/theme/components/Workbench/Card/index.tsx
+++ src/theme/components/Workbench/Card/index.tsx
@@ -1,6 +1,25 @@
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import type { CardViewModel } from '../../../../docusaurus-theme-read-time';
+
+const StyledBox = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: 'rgb(48, 56, 70)',
+    borderRadius: 'var(--space-2xs)',
+    color: 'white',
+    fontSize: 'var(--font-size--0)',
+    padding: 'var(--space-2xs)',
+    '&:hover': {
+        boxShadow: `
+                white 0 0 0rem 0.3rem,
+                rgb(100, 255, 218) 0 0 0rem 0.5rem`,
+    },
+});
 
 interface Props {
     readonly card: CardViewModel;
@@ -12,26 +31,10 @@ export default function Card(
     }: Props
 ): JSX.Element {
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '100%',
-            backgroundColor: 'rgb(48, 56, 70)',
-            borderRadius: 'var(--space-2xs)',
-            color: 'white',
-            fontSize: 'var(--font-size--0)',
-            padding: 'var(--space-2xs)',
-            '&:hover': {
-                boxShadow: `
-                    white 0 0 0rem 0.3rem,
-                    rgb(100, 255, 218) 0 0 0rem 0.5rem`,
-            },
-        }}>
+        <StyledBox>
             <span>{card.targetId}</span>
             <span>{card.details}</span>
             <span>{`${card.readTime.minute}:${card.readTime.second}`}</span>
-        </Box>
+        </StyledBox>
     );
 };
