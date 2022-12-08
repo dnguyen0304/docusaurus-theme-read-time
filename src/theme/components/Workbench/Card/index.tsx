d3761f480e83e07fd5c91ba2209782d5cf99dce5
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-08 15:50:15 +0100

    Migrate styles to sx for Workbench/Card theme component.

--- src/theme/components/Workbench/Card/index.tsx
+++ src/theme/components/Workbench/Card/index.tsx
@@ -1,6 +1,6 @@
+import Box from '@mui/material/Box';
 import * as React from 'react';
 import type { CardViewModel } from '../../../../docusaurus-theme-read-time';
-import styles from './styles.module.css';
 
 interface Props {
     readonly card: CardViewModel;
@@ -12,10 +12,26 @@ export default function Card(
     }: Props
 ): JSX.Element {
     return (
-        <div className={styles.Card_container}>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+            padding: 'var(--space-2xs)',
+            borderRadius: 'var(--space-2xs)',
+            backgroundColor: 'rgb(48, 56, 70)',
+            color: 'white',
+            fontSize: 'var(--font-size--0);           ',
+            '&:hover': {
+                'boxShadow': `
+                    white 0 0 0rem 0.3rem,
+                    rgb(100, 255, 218) 0 0 0rem 0.5rem`,
+            },
+        }}>
             <span>{card.targetId}</span>
             <span>{card.details}</span>
             <span>{card.readTime}</span>
-        </div>
+        </Box>
     );
 };
