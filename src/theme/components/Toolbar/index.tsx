d133709aa0edd3ee69006993e25f896a513d7cf9
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-09 04:15:04 +0100

    Add StyledBox for Toolbar.

--- src/theme/components/Toolbar/index.tsx
+++ src/theme/components/Toolbar/index.tsx
@@ -1,8 +1,20 @@
 import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import { useToolbar } from '../../../contexts/toolbar';
+
+// TODO(dnguyen0304): Add paddingRight for the scrollbar.
+const StyledBox = styled(Box)({
+    // TODO(dnguyen0304): Fix missing responsive design.
+    width: '110px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderLeft: '1px solid var(--ifm-toc-border-color)',
+    paddingTop: 'var(--space-s)',
+});
 
 interface Props { };
 
@@ -13,21 +25,10 @@ export default function Toolbar(
     const { setWorkbenchIsOpen } = useToolbar();
 
     return (
-        <Box
-            // TODO(dnguyen0304): Add paddingRight for the scrollbar.
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                borderLeft: '1px solid var(--ifm-toc-border-color)',
-                paddingTop: 'var(--space-s)',
-                // TODO(dnguyen0304): Fix missing responsive design.
-                width: '110px',
-            }}
-        >
+        <StyledBox>
             <IconButton onClick={() => { setWorkbenchIsOpen(prev => !prev) }}>
                 <InsightsOutlinedIcon />
             </IconButton>
-        </Box>
+        </StyledBox>
     );
 };
